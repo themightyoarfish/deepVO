c9ffa42ce091e4ec97b53dfe9e00b132547671a3
Rasmus Diederichsen <rdiederichse@uos.de>
2018-02-19 16:53:27 +0100

    foo

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from math import ceil
+from tensorflow.contrib.rnn import *
 
 class VOModel(object):
 
@@ -15,22 +16,31 @@ class VOModel(object):
         with tf.name_scope('inputs'):
             h, w, c = image_shape
             # TODO: Resize images before stacking. Maybe do that outside of the graph?
-            self.input_images = tf.placeholder(tf.uint8, shape=[None, sequence_length, h, w, c],
+            self.input_images = tf.placeholder(tf.uint8, shape=[None, sequence_length, h, w, 2 * c],
                                                name='imgs')
-            self.target_poses = tf.placeholder(tf.float32, shape=[6], name='poses')
-            self.batch_size   = tf.placeholder(tf.uint8, shape=[], name='batch_size')
+            self.target_poses = tf.placeholder(tf.float32, shape=[None, sequence_length, 6],
+                                               name='poses')
+            self.batch_size   = tf.placeholder(tf.int32, shape=[], name='batch_size')
             self.hidden_state = tf.placeholder(tf.float32, shape=(None, memory_size),
                                                name='hidden_state')
             self.cell_state   = tf.placeholder(tf.float32, shape=(None, memory_size),
                                                name='cell_state')
+            self.sequence_length = sequence_length
+
         with tf.name_scope('cnn'):
-            self.build_cnn()
+            ksizes     = [7,  5,   5,   3,   3,   3,   3,   3,   3]
+            strides    = [2,  2,   2,   1,   2,   1,   2,   1,   2]
+            n_channels = [64, 128, 256, 256, 512, 512, 512, 512, 1024]
+            self.build_cnn(ksizes, strides, n_channels)
 
-        with tf.name_scope('rnn'):
-            self.build_rnn()
+        # with tf.name_scope('rnn'):
+        #     self.build_rnn(memory_size)
 
-    def build_cnn(self, use_dropout=False):
+    def build_cnn(self, ksizes, strides, n_channels, use_dropout=False):
         '''Create all the conv layers as specified in the paper.'''
+
+        assert len(ksizes) == len(strides) == len(n_channels), ('Kernel, stride and channel specs '
+                                                                'must have same length')
 
         # biases initialise with a small constant
         bias_initializer = tf.constant_initializer(0.01)
@@ -38,10 +48,6 @@ class VOModel(object):
         # kernels initialise according to He et al.
         def kernel_initalizer(k):
             return tf.random_normal_initializer(stddev=np.sqrt(2 / k))
-
-        ksizes     = [7,  5,   5,   3,   3,   3,   3,   3,   3]
-        strides    = [2,  2,   2,   1,   2,   1,   2,   1,   2]
-        n_channels = [64, 128, 256, 256, 512, 512, 512, 512, 1024]
 
         next_layer_input = self.input_images
         for index, ksize, stride, channels in enumerate(zip(ksizes, strides, n_channels)):
@@ -51,33 +57,40 @@ class VOModel(object):
                 next_layer_input = tf.layers.conv2d(next_layer_input,
                                                     channels,
                                                     kernel_size=[ksize, ksize],
-                                                    stride,
+                                                    stride=stride,
                                                     padding='SAME',
                                                     activation=activation,
                                                     kernel_initalizer=kernel_initalizer(ksize),
                                                     bias_initializer=bias_initializer)
         self.conv = next_layer_input
 
-    def build_rnn(self):
-        '''Create all recurrent layers as specified in the paper.'''
-        pass
+    # def build_rnn(self, memory_size, use_dropout=False):
+    #     '''Create all recurrent layers as specified in the paper.'''
+    #     lstm1           = LSTMCell(memory_size)
+    #     lstm2           = LSTMCell(memory_size)
+    #     rnn             = MultiRNNCell([lstm1, lstm2])
 
-    @staticmethod
-    def resize_to_multiple(images, multiples):
-        '''Resize a batch of images in the height and width dimensions so their size are an integer
-        multiple of some value.
+    #     self.zero_state = rnn.zero_state(self.batch_size, tf.float32)
+    #     state           = LSTMStateTuple(c=self.cell_state, h=self.hidden_state)
 
-        Parameters
-        ----------
-        images  :   tf.Tensor
-                    Tensor of shape [batch, height, width, channels]
-        multiples   :   int or tuple
-                        The value/s that should evenly divide the resized image's dimensions
-        '''
-        _, h, w, _ = images.get_shape()
-        # if only one multiple, assume it's the value to use for all dims
-        if not isinstance(multiples, tuple):
-            multiples = multiples * 2
-        new_h, new_w = [int(ceil(input_shape[0] / multiples[0])),
-                        int(ceil(input_shape[1] / multiples[1]))]
-        return tf.image.resize_images(images, [new_h, new_w])
+    #     b, h, w,c = self.conv.get_shape()
+    #     conv_outputs = tf.reshape(self.conv, [b, self.sequence_length, h * w * c])
+    #     outputs, state  = static_rnn(rnn, conv_outputs, dtype=tf.float32, initial_state=state)
+    #     outputs_rehsaped = tf.reshape(outputs, )
+
+    def get_zero_state(self, session, batch_size):
+        return session.run(self.zero_state, feed_dict={self.batch_size: batch_size})
+
+    def propagate_input(self, session, input_batch, label_batch, initial_state=None):
+        batch_size, sequence_length = input_batch.shape[:2]
+
+        if initial_state is None:
+            initial_state = self.get_zero_state(session, batch_size)
+
+        session.run(self.conv, feed_dict={self.input_images: input_batch,
+                                          self.target_poses: label_batch,
+                                          self.batch_size: batch_size,
+                                          self.hidden_state: initial_state.h,
+                                          self.cell_state: initial_state.c,
+                                          self.sequence_length: sequence_length})
+
