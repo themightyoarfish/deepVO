ee91d6f31857e9c9c9a7383c765a345871d8d5aa
Rasmus Diederichsen <rdiederichse@uos.de>
2018-02-20 17:54:42 +0100

    Push some stuff through cnn

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 import numpy as np
+from argparse import ArgumentParser
+import tensorflow as tf
 
 from model import VOModel
-import tensorflow as tf
+from utils import image_pairs
 
 
 def main():
@@ -11,17 +13,19 @@ def main():
     parser.add_argument('-p', '--poses', type=str, required=True, help='Path to pose preprocessed npy file')
     args = parser.parse_args()
     images = np.load(args.imgs)     # shape (N, w, h)
-    poses = np.load(args.poses)     # shape (N, 7)
+    poses = np.load(args.poses)[..., :6]     # shape (N, 7)
 
-    batch_size = 50
     _, h, w, c = images.shape
     image_shape = (h, w, c)
     memory_size = 1000
     sequence_length = 10
+    input_generator = image_pairs(images, sequence_length)
+    input_data, input_labels = next(input_generator), poses[1:1 + sequence_length]
 
     model = VOModel(image_shape, memory_size, sequence_length)
     with tf.Session() as session:
-        zero_state = model.get_zero_state(session, batch_size)
+        session.run(tf.global_variables_initializer())
+        model.get_cnn_output(session, input_data[np.newaxis, ...], input_labels)
 
 
 if __name__ == "__main__":
