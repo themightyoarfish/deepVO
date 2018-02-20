ee3e5b11c6c23e4493639499b21d6f6d869afb87
Rasmus Diederichsen <rdiederichse@uos.de>
2018-02-20 19:01:37 +0100

    Reintroduce the rnn part

--- model.py
+++ model.py
@@ -27,13 +27,14 @@ class VOModel(object):
             self.input_images = tf.placeholder(tf.float32, shape=[None, sequence_length, h, w, 2 * c],
                                                name='imgs')
 
+
             self.target_poses = tf.placeholder(tf.float32, shape=[None, sequence_length, 6],
                                                name='poses')
             self.batch_size   = tf.placeholder(tf.int32, shape=[], name='batch_size')
-            # self.hidden_state = tf.placeholder(tf.float32, shape=(None, memory_size),
-            #                                    name='hidden_state')
-            # self.cell_state   = tf.placeholder(tf.float32, shape=(None, memory_size),
-            #                                    name='cell_state')
+            self.hidden_state = tf.placeholder(tf.float32, shape=(None, memory_size),
+                                               name='hidden_state')
+            self.cell_state   = tf.placeholder(tf.float32, shape=(None, memory_size),
+                                               name='cell_state')
             self.sequence_length = sequence_length
 
         ############################################################################################
@@ -47,28 +48,28 @@ class VOModel(object):
         for idx in range(sequence_length):
             stacked_image = self.input_images[:, idx, :]
             self.cnn_activations.append(self.cnn(stacked_image,
-                                            ksizes,
-                                            strides,
-                                            n_channels,
-                                            reuse=tf.AUTO_REUSE))
-            # with tf.conrrol_dependencies(self.cnn_activations):
-            #     self.dummy_op = tf.identity(self.input_images)
+                                                 ksizes,
+                                                 strides,
+                                                 n_channels,
+                                                 reuse=tf.AUTO_REUSE))
+
+        rnn_inputs = [tf.reshape(conv, [self.batch_size, tf.reduce_prod(conv.get_shape()[1:])])
+                      for conv in self.cnn_activations]
 
         ############################################################################################
         #                                           LSTM                                           #
         ############################################################################################
-        # with tf.variable_scope('rnn'):
-        #     '''Create all recurrent layers as specified in the paper.'''
-        #     lstm1 = LSTMCell(memory_size)
-        #     lstm2 = LSTMCell(memory_size)
-        #     rnn   = MultiRNNCell([lstm1, lstm2])
+        with tf.variable_scope('rnn'):
+            '''Create all recurrent layers as specified in the paper.'''
+            lstm1 = LSTMCell(memory_size)
+            lstm2 = LSTMCell(memory_size)
+            rnn   = MultiRNNCell([lstm1, lstm2])
 
-        #     self.zero_state = rnn.zero_state(self.batch_size, tf.float32)
-        #     state           = LSTMStateTuple(c=self.hidden_state, h=self.cell_state)
+            self.zero_state = rnn.zero_state(self.batch_size, tf.float32)
+            state           = LSTMStateTuple(c=self.hidden_state, h=self.cell_state)
 
-        #     b, h, w,c = self.conv.get_shape()
-        #     conv_outputs = tf.reshape(self.conv, [b, self.sequence_length, h * w * c])
-        #     outputs, state  = static_rnn(rnn, conv_outputs, dtype=tf.float32, initial_state=state)
+            self.rnn_outputs, self.rnn_state  = static_rnn(rnn, rnn_inputs, dtype=tf.float32,
+                                                           initial_state=state)
 
 
     def cnn(self, input, ksizes, strides, n_channels, use_dropout=False, reuse=True):
@@ -116,6 +117,31 @@ class VOModel(object):
         Batch size (influences the size of the RNN state)
         '''
         return session.run(self.zero_state, feed_dict={self.batch_size: batch_size})
+
+    def get_rnn_output(self, session, input_batch, label_batch, initial_state=None):
+        '''Run some input through the cnn net, followed by the rnn net
+
+        Parameters
+        ----------
+        session :   tf.Session
+        Session to execute op in
+        input_batch  :  np.ndarray
+        Array of shape (batch_size, sequence_length, h, w, 6) were two consecutive
+        rgb images are stacked together.
+        label_batch :   np.ndarray
+        Array of shape (batch_size, sequence_length, 7) with Poses
+        initial_state   :   LSTMStateTuple (aka namedtuple(c,h))
+        Previous state
+        '''
+        batch_size = input_batch.shape[0]
+
+        if initial_state is None:
+            initial_state = self.get_zero_state(session, batch_size)
+
+        return session.run(self.cnn_activations, feed_dict={self.input_images: input_batch,
+                                                            self.target_poses: label_batch,
+                                                            self.batch_size: batch_size})
+
 
     def get_cnn_output(self, session, input_batch, label_batch, initial_state=None):
         '''Run some input through the cnn net.
