915d599d83e666a614c3fbe7e373dfabab813ec9
Rasmus Diederichsen <rdiederichse@uos.de>
2018-03-05 19:23:36 +0100

    Comment

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3.6
+'''
+.. module:: main
+    Module for starting the training, testing and plotting functions
+
+.. moduleauthor:: Rasmus Diederichsen
+'''
 
 import sys
 import numpy as np
